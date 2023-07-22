15c03492c26dc4f01167ed95f22630125cd0efaa
takashi <42316228+k-tokitoh@users.noreply.github.com>
2023-07-22 10:37:38 +0900

    rearrange files

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ```
+$ cd envs
 $ terraform apply
 ```
 
--- envs/main.tf
+++ envs/main.tf
@@ -9,10 +9,6 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
